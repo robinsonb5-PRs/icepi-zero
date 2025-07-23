6408e5c9a8ca0a05de4de86e7ba4c6c909cffbcd
cheyao <cyao@duck.com>
2025-07-23 16:41:52 +0200

    I hate max

--- firmware/counter/counter.sv
+++ firmware/counter/counter.sv
@@ -1,50 +1,36 @@
 module top (
 	input  wire        clk,
-	input  logic       button,
+	input  logic [1:0] button,
 	output logic [4:0] led
 );
-	// Slow clock for debouncing 762Hz
-	logic [11:0] clkb;
+    logic btn, btn_last;
+    debouncer bounce(.clk(clk), .btn(!button[0]), .sig(btn));
 
-	initial begin
-		clkb <= 0;
-	end
-
-	always_ff @(posedge clk) begin 
-		clkb <= clkb + 1'b1;
-	end
-
-	wire clki = clkb[11];
-
-	// Real code
-	logic last_state;
-	logic bounce;
-	logic [3:0] timer;
-	
-	initial begin
-		led <= 0;
-		last_state <= 0;
-		bounce <= 0;
-		timer <= 0;
-	end
-
-	always_ff @(posedge clki) begin
-		// Decrease the counter;
-		if (timer != 0) begin
-			timer <= (timer == 0 ? 0 : timer - 1);
-		end else if (button != last_state) begin 
-			// Button value isn't last state, start timer
-			bounce <= button;
-			timer <= 4'b1111;
-		end
+	always_ff @(posedge clk) begin
+        btn_last <= btn;
 
 		// Timer ended
-		if (timer == 1 && button == bounce) begin
-			last_state <= button;
-
-			if (!button) begin
-				led <= led + 1;
-			end
+		if (btn != btn_last && !btn) begin
+            led <= led + 1;
 		end
 	end
+endmodule
+
+module debouncer #(
+    parameter N = 7
+) (
+    input  clk,
+    input  btn,
+    output sig
+);
+    logic [N-1:0] buffer;
+    assign sig = buffer[N-1];
+
+    always_ff @(posedge clk) begin
+        if (!buffer[N-1] && btn) begin
+            buffer <= buffer + 1;
+        end else if (!btn) begin
+            buffer <= 0;
+        end
+    end
 endmodule
